8ef2c8b72c6c2e22148cf29f5538b7823c99537c
yutao <obayashi21249@cs.saisoncard.co.jp>
2025-06-24 18:34:02 +0900

    テンプレートリテラル内にいるかどうか判定する処理を追加

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,26 @@ export function activate(context: vscode.ExtensionContext) {
 		// Get text up to the cursor
 		const textUpToCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
 		const backtickCount = (textUpToCursor.match(/`/g) || []).length;
-	})
+
+		// Determine whether it is in a template literal
+		// Remain 1 = inside (true), Remain 0 = outside (false)
+		const insideTemplate = backtickCount % 2 === 1;
+
+		if (insideTemplate) {
+			editor.edit(editBuilder => {
+				editBuilder.insert(position, '${}');
+			}).then(() => {
+				const newPosition = position.translate(0, 2);
+				editor.selection = new vscode.Selection(newPosition, newPosition);
+			});
+		} else {
+			editor.edit(editBuilder => {
+				editBuilder.insert(position, '$');
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
